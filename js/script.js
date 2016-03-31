d18f6346fcb39a05817445a19196a078e64d0682
SergeyBruz <bruzgins@gmail.com>
2016-03-31 21:05:33 +0300

    sedona 6

--- js/script.js
+++ js/script.js
@@ -1,61 +1,3 @@
-// http://osvaldas.info/caching-svg-sprite-in-localstorage
-;( function( window, document )
-{
-    'use strict';
-
-    var file     = 'img/sprite_sedona.svg',
-        revision = 1;
-
-    if( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
-        return true;
-
-    var isLocalStorage = 'localStorage' in window && window[ 'localStorage' ] !== null,
-        request,
-        data,
-        insertIT = function()
-        {
-            document.body.insertAdjacentHTML( 'afterbegin', data );
-        },
-        insert = function()
-        {
-            if( document.body ) insertIT();
-            else document.addEventListener( 'DOMContentLoaded', insertIT );
-        };
-
-    if( isLocalStorage && localStorage.getItem( 'inlineSVGrev' ) == revision )
-    {
-        data = localStorage.getItem( 'inlineSVGdata' );
-        if( data )
-        {
-            insert();
-            return true;
-        }
-    }
-
-    try
-    {
-        request = new XMLHttpRequest();
-        request.open( 'GET', file, true );
-        request.onload = function()
-        {
-            if( request.status >= 200 && request.status < 400 )
-            {
-                data = request.responseText;
-                insert();
-                if( isLocalStorage )
-                {
-                    localStorage.setItem( 'inlineSVGdata',  data );
-                    localStorage.setItem( 'inlineSVGrev',   revision );
-                }
-            }
-        }
-        request.send();
-    }
-    catch( e ){}
-
-}( window, document ) );
-
-
 ;( function( window, document )
 {
   var link = document.querySelector(".btn--title");
@@ -77,85 +19,145 @@
       search_form.classList.remove("main_footer__search_form--show");
     }
 });
-  //календарь
-  var calendar = document.querySelector(".datepicker");
-  var datepicker_open = document.querySelector(".calendar_icon");
-  datepicker_open.addEventListener("click", function(event) {
+//мобильное меню
+mobile_menu_open.addEventListener("click", function(event) {
+  event.preventDefault();
+  var mobile_menu = document.querySelectorAll(".item--mobile_menu");
+  for (var i = 0; i < mobile_menu.length; i++) {
+    mobile_menu[i].classList.add("item--mobile_menu_show");
+  }
+});
+mobile_menu_close.addEventListener("click", function(event) {
+  event.preventDefault();
+  var mobile_menu = document.querySelectorAll(".item--mobile_menu");
+  for (var i = 0; i < mobile_menu.length; i++) {
+    mobile_menu[i].classList.remove("item--mobile_menu_show");
+  }
+});
+}( window, document ) );
+
+
+//JQuery
+
+  $(document).ready(function() {
+
+
+    $(".calendar_icon--in").click(function(){
+        $('.datepicker_in').toggle();
+    });
+    $(".calendar_icon--out").click(function(){
+        $('.datepicker_out').toggle();
+    });
+
+    //взрослые
+    $('.min').click(function () {
+      var $input = $(this).parent().find('#adults');
+      var count = parseInt($input.val()) - 1;
+      count = count < 1 ? 1 : count;
+      $input.val(count);
+      $input.change();
+      return false;
+    });
+    $('.max').click(function () {
+      var $input = $(this).parent().find('#adults');
+      $input.val(parseInt($input.val()) + 1);
+      $input.change();
+      return false;
+    });
+    //дети
+    $('.min').click(function () {
+      var $input = $(this).parent().find('#children');
+      var count = parseInt($input.val()) - 1;
+      count = count < 0 ? 0 : count;
+      $input.val(count);
+      $input.change();
+      return false;
+    });
+    $('.max').click(function () {
+      var $input = $(this).parent().find('#children');
+      $input.val(parseInt($input.val()) + 1);
+      $input.change();
+      return false;
+    });
+
+//календарь
     $(function(){
       $.datepicker.setDefaults(
             $.extend($.datepicker.regional["ru"])
       );
-      $("#datepicker").datepicker({
-        minDate: "+1",
-        altField:"#date_in",
-        altFormat:"dd.mm.yy",
-        onSelect: function(datepicker, j){
+    })
+    $(function(){
+        $( ".datepicker_in" ).datepicker({
+            inline: true, //Отображать в div вместо input
+            defaultDate: "+0d",
+            numberOfMonths: 1,
+            dateFormat:"dd.mm.yy",
+            altFormat: "dd M yy",
+            beforeShowDay: allowedDates, //Функция блокировки дат
+            //Поле формы, куда записывать значение календаря
+            altField:"#date_in",
+            //Переставляем дату во втором календаре
+            onSelect: function( selectedDate ) {
+                $(".datepicker_out" ).datepicker( "option", "minDate", selectedDate ).css('display','block');
+                $(".datepicker_in").css('display','none');
+            }
+        });
 
-          j=1;
-          return j;
+        //Второй календарь, с конечной датой
+        $( ".datepicker_out" ).datepicker({
+            inline: true,
+            defaultDate: "+0d",
+            numberOfMonths: 1,
+            altFormat: "dd M yy",
+            dateFormat:"dd.mm.yy",
+            beforeShowDay: allowedDates,
+            altField:"#date_out",
+            //Переставляем дату в первом календаре
+            onSelect: function( selectedDate ) {
+                $(".datepicker_in" ).datepicker( "option", "maxDate", selectedDate );
+                $(".datepicker_out").css('display','none');
+                $("#adults").focus();
+            }
+        });
+        $(document).mouseup(function (e) {
+            var container = $(".datepicker_in");
+            if (container.has(e.target).length === 0){
+                container.hide();
+            }
+        });
+        $(document).mouseup(function (e) {
+            var container = $(".datepicker_out");
+            if (container.has(e.target).length === 0){
+                container.hide();
+            }
+        });
+
+    });
+
+    //Массив с датами, которые нужно сделать недоступными
+    var jsForbiddenDatesArray={}
+    jsForbiddenDatesArray=[new Date("8/30/2012"),new Date("8/31/2012"),new Date("8/24/2012")];
+
+    //Функция которая делает даты недоступными
+    function allowedDates(date) {
+
+        now=new Date();
+        now.setHours(0);
+        now.setMinutes(0);
+        now.setSeconds(0);
+        now.setMilliseconds(0);
+
+        //Исключаем даты раньше сегодняшнего дня
+        if (date.getTime() < now.getTime()) return [false, ''];
+
+        //Исключаем даты из массива
+        for (var i = 0; i < jsForbiddenDatesArray.length; i++) {
+            if (date.getTime() == jsForbiddenDatesArray[i].getTime())
+                return [false, ''];
         }
-      });
-    });
+        //Если нужно исключить выходные дни раскомментируйте строку ниже
+        //return jQuery.datepicker.noWeekends(date);
+        return [true,''];
+    }
 
-    if (j==0){
-      event.preventDefault();
-      calendar.classList.add("datepicker--show");
-      j=1;
-    }
-    else if(j==1){
-      j=0;
-      event.preventDefault();
-      calendar.classList.remove("datepicker--show");
-    }
-});
-  //мобильное меню
-  mobile_menu_open.addEventListener("click", function(event) {
-    event.preventDefault();
-    var mobile_menu = document.querySelectorAll(".item--mobile_menu");
-    for (var i = 0; i < mobile_menu.length; i++) {
-      mobile_menu[i].classList.add("item--mobile_menu_show");
-    }
-});
-  mobile_menu_close.addEventListener("click", function(event) {
-    event.preventDefault();
-    var mobile_menu = document.querySelectorAll(".item--mobile_menu");
-    for (var i = 0; i < mobile_menu.length; i++) {
-      mobile_menu[i].classList.remove("item--mobile_menu_show");
-    }
   });
-}( window, document ) );
-
-  //форма поиска элементы
-    $(document).ready(function() {
-
-      //взрослые
-        $('.min').click(function () {
-            var $input = $(this).parent().find('#adults');
-            var count = parseInt($input.val()) - 1;
-            count = count < 1 ? 1 : count;
-            $input.val(count);
-            $input.change();
-            return false;
-        });
-        $('.max').click(function () {
-            var $input = $(this).parent().find('#adults');
-            $input.val(parseInt($input.val()) + 1);
-            $input.change();
-            return false;
-        });
-      //дети
-        $('.min').click(function () {
-            var $input = $(this).parent().find('#children');
-            var count = parseInt($input.val()) - 1;
-            count = count < 0 ? 0 : count;
-            $input.val(count);
-            $input.change();
-            return false;
-        });
-        $('.max').click(function () {
-            var $input = $(this).parent().find('#children');
-            $input.val(parseInt($input.val()) + 1);
-            $input.change();
-            return false;
-        });
-    });
